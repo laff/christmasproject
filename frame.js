dfffa3834a7669a7e411021338529c931b7b52ad
olafab <olafab@ifi.uio.no>
2016-03-05 00:02:11 +0100

    first column lenghts set

--- frame.js
+++ frame.js
@@ -14,7 +14,9 @@ Frame.prototype = {
 	// this is for scaling the svg
 	scale: null,
 
-	structure: null,
+	// These two should be higher up the chain/ available for options.
+	spacing: 10,
+	border: 50,
 
 	coordinates: {
 
@@ -50,8 +52,8 @@ Frame.prototype = {
 
 	createColumns: function () {
 
-		var spacing = 10,
-			border = 50,
+		var spacing = this.spacing,
+			border = this.border,
 			cols = bounds.structure.length,
 			width = this.viewbox.width - (50 * 2),
 			height = this.viewbox.height - (50 * 2),
@@ -66,13 +68,12 @@ Frame.prototype = {
 			max,
 			frames = [];
 
-
 		// starting off easy, brain hurts
 		if (cols == 2) {
 
 			part = (width * .2);
-			min = Math.floor((part * 2));
-			max = Math.floor((part * 2));
+			min = (part * 2);
+			max = (part * 2);
 			part = rand(min, max);
 
 
@@ -108,19 +109,75 @@ Frame.prototype = {
 		}
 	},
 
+	/**
+	 *	Method that is in charge of creating rows within the columns.
+	**/
 	createRows: function () {
 
 		var coord = this.coordinates,
+			rowCount = bounds.structure,
+			// height of both columns is the same
+			height = (coord.cols[0][1].y- coord.cols[0][0].y),
+			width,
+			spacing = this.spacing,
 			c = coord.cols.length,
 			i = 0;
 
-		console.log(c);
+		/**
+		 *	Function that creates rows within a column.
+		**/
+		function rowCreate (nr) {
 
-		for (i; i < c; i++) {
+			var rows = rowCount[nr],
+				// the height of the svg minus the spacings
+				newHeight = (height - (spacing * rows)),
 
-			
-			
+				// each part height before random add.
+				part = ((newHeight * .7) / rows),
+
+				// what is left over, the maximum of adds that can be made
+				diff = (newHeight * .3),
+
+				// array storing lengths found
+				lengths = [];
+
+			/**
+			 *	Function that decides the lenghts of each left side of the column.
+			 *
+			 *	Stores in lengths array.
+			**/
+			function randoLength (rowNr, left, tmp) {
+
+				var r = rand(0, left);
+
+				if (tmp) {
+					lengths.push(tmp);
+				}
+
+				return (left > 0) ?
+					(rowNr < (rows - 1)) ?
+						randoLength((rowNr + 1), (left - r), (part + r)) :
+					randoLength((rowNr + 1), 0, (part + left)) :
+				null;
+			}
+
+			randoLength(0, diff);
+
+			console.log(lengths);
+
+
+			// Start off on top
+			// how to divide in x rows.
+			// divide in x parts same size, abstract 20%. add 0-20% each until filled.
+
+
+
+
+
+			return (nr < (c - 1)) ? rowCreate(nr + 1) : null;
 		}
+
+		rowCreate(0);
 
 	}
 }
